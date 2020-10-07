e682eca852ba3e62b7e8b8c397069221db9acdd9
joyadas1999 <47679860+joyadas1999@users.noreply.github.com>
2020-10-06 20:37:30 -0400

    updated

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,34 @@
 import React from "react";
 
+import Header from "../components/Header";
+
+const weatherKey = `3206233bce5e109446709fd59f166d4f`;
 function Home() {
+  /* Display:
+Weather Type (ex. Cloudy)
+Current Temperature
+High Temperature
+Low Temperature
+Cloudiness
+Humidity
+Wind Speed
+*/
   return (
-    <div>
-      <h1>Weather App</h1>
-    </div>
+    <>
+      <Header />
+      <main className="Home">
+        <h2>Weather in Seoul</h2>
+        <div classname="Weather Info">
+          <p>Weather Type: Cloudy</p>
+          <p>Current Temperature: 100 degrees</p>
+          <p>High Temperature: 100 degrees</p>
+          <p>Low Temperature: 80 degrees</p>
+          <p>Cloudiness: 100</p>
+          <p>Humidity: 35%</p>
+          <p>Wind Speed: 3km/h</p>
+        </div>
+      </main>
+    </>
   );
 }
 
